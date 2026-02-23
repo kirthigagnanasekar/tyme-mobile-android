322188f94297f3dad09c26e0b209743d230a7ed1
kirthigagnanasekar <kirthiga.g@spritle.com>
2026-02-23 16:20:13 +0530

    Feat: Modified locator in edit profile

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/feature/LoginAndUpdateProfile.feature",
+        features = "src/test/resources/feature",
         glue = "stepDefinitions",
         plugin = {
                 "summary",
@@ -14,7 +14,10 @@ import org.junit.runner.RunWith;
                 "html:target/cucumber-reports/cucumber-report.html",
                 "json:target/cucumber-reports/cucumber-report.json",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-        }
+        },
+
+        monochrome = true,
+        publish = true
 )
 public class TestRunner {
 }
